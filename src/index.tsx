fed117f21797e7f52f0200bf1e8faeb298730f6b
gersonDantas <gerson.accari@hotmail.com>
2021-11-06 10:16:22 -0300

    netlify

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+import { IonReactHashRouter } from '@ionic/react-router';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <IonReactHashRouter
+      basename="/"
+    >
+      <App />
+    </IonReactHashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
