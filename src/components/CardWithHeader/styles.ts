4a42fcb7cbf1f312b55a680a803b5f8a93871a20
gersonDantas <gerson.accari@hotmail.com>
2021-11-07 00:02:01 -0300

    firebase update

--- src/components/CardWithHeader/styles.ts
+++ src/components/CardWithHeader/styles.ts
@@ -27,5 +27,8 @@ export const IonCardHeaderTitle = styled(IonCardTitle)`
   font-family: "Inter", sans-serif;
   font-weight: 500;
   font-size: 14px;
+  @media screen and (max-width: 540px) {
+    height: 25px;
+  }
 `;
 
